4a080f779c0e4060dfbd2f3a35b7e396b8699e2e
fp <342834999@qq.com>
2018-07-25 16:38:59 +0800

    页面修改

--- src/pages/tabsdemo/tabs/tabs.ts
+++ src/pages/tabsdemo/tabs/tabs.ts
@@ -8,34 +8,15 @@ import {Events, IonicPage, NavController, NavParams, Tabs} from 'ionic-angular';
   templateUrl: 'tabs.html',
 })
 export class TabsPage {
-  @ViewChild('myTabs') tabRef: Tabs;
   tab1Root: any = 'AboutPage';
   tab2Root: any = 'ContactPage';
   tab3Root: any = 'FormsPage';
-  mb: any;
-  constructor(private event: Events, private renderer: Renderer2, private elementRef: ElementRef,) {
+  constructor( ) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad TabsPage');
-    let tabs = this.queryElement(this.elementRef.nativeElement, '.tabbar');
-    this.event.subscribe('hideTabs', () => {
-      this.renderer.setStyle(tabs, 'display', 'none');
-      let SelectTab = this.tabRef.getSelected()._elementRef.nativeElement;
-      let content = this.queryElement(SelectTab, '.scroll-content');
-      this.mb = content.style['margin-bottom'];
-      this.renderer.setStyle(content, 'margin-bottom', '0')
-    });
-    this.event.subscribe('showTabs', () => {
-      this.renderer.setStyle(tabs, 'display', '');
-      let SelectTab = this.tabRef.getSelected()._elementRef.nativeElement;
-      let content = this.queryElement(SelectTab, '.scroll-content');
-      this.renderer.setStyle(content, 'margin-bottom', this.mb)
-    })
+
 
   }
 
-  queryElement(elem: HTMLElement, q: string): HTMLElement {
-    return <HTMLElement>elem.querySelector(q);
-  }
 }
